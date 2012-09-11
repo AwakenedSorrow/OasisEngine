2b2e2ae97df38dd197f223fb7c4764594467ca5f
Stein <Silvh92@gmail.com>
2012-09-11 10:37:38 +0300

    Declare WINDOW_TITLE

--- Server/OasisServer/OasisServer/Data/modGlobals.vb
+++ Server/OasisServer/OasisServer/Data/modGlobals.vb
@@ -1,11 +1,9 @@
 ﻿Module modGlobals
-    ' ***
     ' Server Threads
-    ' ***
     Public SLoop As Threading.Thread = New Threading.Thread(AddressOf ServerLoop) ' Main Server Thread
 
-    ' ***
     ' General Server Information
-    ' ***
     Public IsRunning As Boolean = False
+    Public WINDOW_TITLE as String
+
 End Module
