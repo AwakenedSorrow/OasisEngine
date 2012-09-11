50396e672973238c71e3209b24c55eed91719235
Stein <Silvh92@gmail.com>
2012-09-11 10:32:49 +0300

    Removing some random comment stars.

--- Server/OasisServer/OasisServer/Logic/modServerLoop.vb
+++ Server/OasisServer/OasisServer/Logic/modServerLoop.vb
@@ -1,22 +1,16 @@
 ﻿Module modServerLoop
 
     Public Sub ServerLoop()
-        ' ***
         ' We just entered this sub.
         ' Should probably set the loop to actually be ON.
-        ' ***
         IsRunning = True
 
-        ' ***
         ' Main Server Loop
-        ' ***
         Do While IsRunning
 
         Loop
 
-        ' ***
         ' Close the Server Loop Thread
-        ' ***
         SLoop.Abort()
     End Sub
 
